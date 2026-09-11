88a7224bdc66dd26b4903771dacb9f680d66b07f
xm0dx <bobmoon812@gmail.com>
2026-09-11 09:55:13 +0200

    Update Main method

---
+++ Account.cs
@@ -0,0 +1,6 @@
+﻿namespace SimpeltGit;
+
+public class Account
+{
+
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpeltGit;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    }
+}
