27dfbc495d5cde869b12768909f4c78fd93e5710
xm0dx <bobmoon812@gmail.com>
2026-09-11 13:05:53 +0200

    Added Deposit method

--- Account.cs
+++ Account.cs
@@ -5,4 +5,9 @@ public class Account
     public int Balance { get; set; }
 
     public string AccountNummer { get; set; } = " ";
+
+    public void Deposit(int amount)
+    {
+        Balance += amount;
+    }
 }
